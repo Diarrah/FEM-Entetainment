801d78912cadf10828ea7eedfd25e84a6e373e64
Diarrah <diarrahcasiano@gmail.com>
2022-04-14 18:21:20 -0700

    React Router linking main pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Movies, TV, Bookmarked } from './MainPages';
+
 const App = () => {
     return (
-        <div className="App">
-            Hello
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/tv-series" element={<TV />} />
+                    <Route path="/bookmarks" element={<Bookmarked />} />
+                </Routes>
+            </div>
+        </Router>
+        
     )
 }
 
