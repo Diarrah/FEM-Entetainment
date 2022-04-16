f041031cf73ad2268fe1aa4d8b28da8ebd3a1b60
Diarrah <diarrahcasiano@gmail.com>
2022-04-16 13:15:55 -0700

    Skeleton on trending section (mapping JSON)

--- src/MainPages/Home.js
+++ src/MainPages/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import data from '../data.json';
+import { tv, movie } from '../assets';
 
 const Home = () => {
+    //const [allMedia, setAllMedia] = useState([]);
+    console.log(data)
     return (
         <div className="Home">
-            Home page
+            <div className="trending-container">
+                {data
+                    .filter(media => (media.isTrending))
+                    .map(media => (
+                        <div className="trending">
+                            <p className="trending__data">
+                                <span className="trending__data--year">{media.year}</span> ·
+                                <span className="trending__data--icon">
+                                   <img src={media.category === 'Movie' ? movie : tv} alt={`${media.category} icon`}/> 
+                                   {media.category}
+                                </span> ·
+                                <span className="trending__data--rating">{media.rating}</span>
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className="recommended">
+                recommended for you
+            </div>
         </div>
     )
 }
