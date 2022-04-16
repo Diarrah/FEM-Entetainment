d3fd7aa32824a0952fc5cd20af6c201afe5802fd
Diarrah <diarrahcasiano@gmail.com>
2022-04-16 12:29:34 -0700

    Grouping images for easy import

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+// Category
+export { default as tv } from './icon-category-tv.svg';
+export { default as movie } from './icon-category-movie.svg';
+
+// Navbar
+export { default as navBookmark } from './icon-nav-bookmark.svg';
+export { default as home } from './icon-nav-home.svg';
+export { default as navMovies } from './icon-nav-movies.svg';
+export { default as navTv } from './icon-nav-tv-series.svg';
+
+// Miscellaneous
+export { default as bookmarkEmpty } from './icon-bookmark-empty.svg';
+export { default as bookmarkFull } from './icon-bookmark-full.svg';
+export { default as play } from './icon-play.svg';
+export { default as search } from './icon-search.svg';
+export { default as user } from './image-avatar.png';
