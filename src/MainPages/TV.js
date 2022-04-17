d8e3beb63a97fcf2c1a26b5e257805506603b3f8
Diarrah <diarrahcasiano@gmail.com>
2022-04-17 13:53:26 -0700

    Filtering and mapping TV shows from JSON

--- src/MainPages/TV.js
+++ src/MainPages/TV.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { tv, bookmarkEmpty, bookmarkFull } from "../assets";
+import data from '../data.json';
 
 const TV = () => {
+    const [series] = useState([...data.filter(series => (series.category === 'TV Series'))]);
+
     return (
-        <div className="tv">
-            TV page
+        <div className="shows">
+            <h1>TV Series</h1>
+            {series.map(show => (
+                <div className="show">
+                    <img 
+                        className="show__bookmark" 
+                        src={show.isBookmarked ? bookmarkFull : bookmarkEmpty} 
+                        alt={`${show.isBookmarked ? 'Full' : 'Empty'} bookmark icon`}
+                    />
+                    <p className="show__data">
+                        <span className="show__data--year">{show.year}</span> ·
+                        <span className="show__data--icon">
+                            <img src={tv} alt="show icon" /> {show.category}
+                        </span> ·
+                        <span className="show__data--rating">{show.rating}</span>
+                    </p>
+                    <h3>{show.title}</h3>
+                </div>
+            ))}
         </div>
     )
 }
